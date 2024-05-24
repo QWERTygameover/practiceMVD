9853ba5d659e3b4b6e1ad4adb33ba16b72ec13bb
KingGod <arsbar91@gmail.com>
2024-05-24 21:41:52 +0700

    asd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿using Oracle.ManagedDataAccess.Client;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Oracle.ManagedDataAccess.Client;
 using System.Text.RegularExpressions;
 using ConsoleApp1;
 
@@ -49,74 +51,66 @@ try
 
         List<Data> dates = Methods.FillDataClasses(allData);
 
-        Data minff = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ff);
-        Data minfl = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.fl);
-        Data minpt = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.pt);
-        Data minlf = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.lf);
-        Data minll = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ll);
-        Data mintp = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.tp);
-        Data mintt = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.tt);
-        Data minffLit = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ffLit);
-        Data minflLit = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.flLit);
-        Data minptLit = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ptLit);
-        Data minlfLit = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.lfLit);
-        Data minllLit = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.llLit);
-        Data mintpLit = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.tpLit);
-        Data minttLit = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ttLit);
+        PortData finalData = new PortData();
+        
+        finalData.ffMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ff).ff;
+        finalData.flMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.fl).fl;
+        finalData.ptMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.pt).pt;
+        finalData.lfMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.lf).lf;
+        finalData.llMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ll).ll;
+        finalData.tpMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.tp).tp;
+        finalData.ttMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.tt).tt;
+        finalData.fflitMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ffLit).ffLit;
+        finalData.fllitMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.flLit).flLit;
+        finalData.ptlitMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ptLit).ptLit;
+        finalData.lflitMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.lfLit).lfLit;
+        finalData.lllitMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.llLit).llLit;
+        finalData.tplitMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.tpLit).tpLit;
+        finalData.ttlitMin = dates.Where(_=>_.date == dateTime).ToArray().MinBy(_=>_.ttLit).ttLit;
 
-        Data maxff = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ff);
-        Data maxfl = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.fl);
-        Data maxpt = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.pt);
-        Data maxlf = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.lf);
-        Data maxll = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ll);
-        Data maxtp = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.tp);
-        Data maxtt = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.tt);
-        Data maxffLit = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ffLit);
-        Data maxflLit = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.flLit);
-        Data maxptLit = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ptLit);
-        Data maxlfLit = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.lfLit);
-        Data maxllLit = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.llLit);
-        Data maxtpLit = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.tpLit);
-        Data maxttLit = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ttLit);
+        finalData.ffMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ff).ff;
+        finalData.flMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.fl).fl;
+        finalData.ptMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.pt).pt;
+        finalData.lfMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.lf).lf;
+        finalData.llMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ll).ll;
+        finalData.tpMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.tp).tp;
+        finalData.ttMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.tt).tt;
+        finalData.fflitMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ffLit).ffLit;
+        finalData.fllitMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.flLit).flLit;
+        finalData.ptlitMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ptLit).ptLit;
+        finalData.lflitMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.lfLit).lfLit;
+        finalData.lllitMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.llLit).llLit;
+        finalData.tplitMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.tpLit).tpLit;
+        finalData.ttlitMax = dates.Where(_=>_.date == dateTime).ToArray().MaxBy(_=>_.ttLit).ttLit;
 
         string result = $"\n{dateTime.Date} \n" +
                         $"{"Cond", 5} {"Min", 5} {"Max", 5} \n" +
-                        $"{"ff", 5} {minff.ff, 5} {maxff.ff, 5} \n" +
-                        $"{"fl", 5} {minfl.fl, 5} {maxfl.fl, 5} \n" +
-                        $"{"pt", 5} {minpt.pt, 5} {maxpt.pt, 5} \n" +
-                        $"{"lf", 5} {minlf.lf, 5} {maxlf.lf, 5} \n" +
-                        $"{"ll", 5} {minll.ll, 5} {maxll.ll, 5} \n" +
-                        $"{"tp", 5} {mintp.tp, 5} {maxtp.tp, 5} \n" +
-                        $"{"tt", 5} {mintt.tt, 5} {maxtt.tt, 5} \n" +
-                        $"{"ffLit", 5} {minffLit.ffLit, 5} {maxffLit.ffLit, 5} \n" +
-                        $"{"flLit", 5} {minflLit.flLit, 5} {maxflLit.flLit, 5} \n" +
-                        $"{"ptLit", 5} {minptLit.ptLit, 5} {maxptLit.ptLit, 5} \n" +
-                        $"{"lfLit", 5} {minlfLit.lfLit, 5} {maxlfLit.lfLit, 5} \n" +
-                        $"{"llLit", 5} {minllLit.llLit, 5} {maxllLit.llLit, 5} \n" +
-                        $"{"tpLit", 5} {mintpLit.tpLit, 5} {maxtpLit.tpLit, 5} \n" +
-                        $"{"ttLit", 5} {minttLit.ttLit, 5} {maxttLit.ttLit, 5} \n";
+                        $"{"ff", 5} {finalData.ffMin, 5} {finalData.ffMax, 5} \n" +
+                        $"{"fl", 5} {finalData.flMin, 5} {finalData.flMax, 5} \n" +
+                        $"{"pt", 5} {finalData.ptMin, 5} {finalData.ptMax, 5} \n" +
+                        $"{"lf", 5} {finalData.lfMin, 5} {finalData.lfMax, 5} \n" +
+                        $"{"ll", 5} {finalData.llMin, 5} {finalData.llMax, 5} \n" +
+                        $"{"tp", 5} {finalData.tpMin, 5} {finalData.tpMax, 5} \n" +
+                        $"{"tt", 5} {finalData.ttMin, 5} {finalData.ttMax, 5} \n" +
+                        $"{"ffLit", 5} {finalData.fflitMin, 5} {finalData.fflitMax, 5} \n" +
+                        $"{"flLit", 5} {finalData.fllitMin, 5} {finalData.fllitMax, 5} \n" +
+                        $"{"ptLit", 5} {finalData.ptlitMin, 5} {finalData.ptlitMax, 5} \n" +
+                        $"{"lfLit", 5} {finalData.lflitMin, 5} {finalData.lflitMax, 5} \n" +
+                        $"{"llLit", 5} {finalData.lllitMin, 5} {finalData.lllitMax, 5} \n" +
+                        $"{"tpLit", 5} {finalData.tplitMin, 5} {finalData.tplitMax, 5} \n" +
+                        $"{"ttLit", 5} {finalData.ttlitMin, 5} {finalData.ttlitMax, 5} \n";
                         
         StreamWriter writer = new StreamWriter(savePath, true);
 
         writer.Write(result);
 
         writer.Close();
-        
-        OracleConnection connection = new OracleConnection(connectionString);
 
-        connection.Open();
-        
-        OracleCommand cmd = connection.CreateCommand();
-
-        cmd.CommandText = $"Insert into Datas (ffmin, ffmax, flmin, flmax, ptmin, ptmax, lfmin, lfmax, llmin, llmax, tpmin, tpmax, ttmin, ttmax, fflitmin, fflitmax, fllitmin, fllitmax, ptlitmin, ptlitmax, lflitmin, lflitmax, lllitmin, lllitmax, tplitmin, tplitmax, ttlitmin, ttlitmax, dateofreport) values({minff.ff}, {maxff.ff}, {minfl.fl}, {maxfl.fl},{minpt.pt}, {maxpt.pt},{minlf.lf}, {maxlf.lf},{minll.ll}, {maxll.ll},{mintp.tp}, {maxtp.tp},{mintt.tt}, {maxtt.tt},{minffLit.ffLit}, {maxffLit.ffLit},{minflLit.flLit}, {maxflLit.flLit},{minptLit.ptLit}, {maxptLit.ptLit},{minlfLit.lfLit}, {maxlfLit.lfLit},{minllLit.llLit}, {maxllLit.llLit},{mintpLit.tpLit}, {maxtpLit.tpLit},{minttLit.ttLit}, {maxttLit.ttLit}, '{dateTime.Day}-{dateTime.Month}-{dateTime.Year}')";
-
-        cmd.ExecuteNonQuery();
-        
-        connection.Close();
+        PostDataToDB(finalData);
         
         Console.WriteLine("Успешно!");
         
-        Thread.Sleep(TimeSpan.FromDays(1));
+        Thread.Sleep(TimeSpan.FromSeconds(10));
         
     } while (true);
     
@@ -136,4 +130,45 @@ catch (Exception e)
     writer.Write(message);
 
     writer.Close();
+}
+
+void PostDataToDB(PortData finalData)
+{
+    HttpClient client = new HttpClient();
+
+    JsonContent jsonContent = JsonContent.Create(finalData);
+    client.PostAsync("http://localhost:5122/AddData", jsonContent);
+}
+
+public class PortData
+{
+    public int ffMin { get; set; }
+    public int ffMax { get; set; }
+    public int flMin { get; set; }
+    public int flMax { get; set; }
+    public int ptMin { get; set; }
+    public int ptMax { get; set; }
+    public int lfMin { get; set; }
+    public int lfMax { get; set; }
+    public int llMin { get; set; }
+    public int llMax { get; set; }
+    public int tpMin { get; set; }
+    public int tpMax { get; set; }
+    public int ttMin { get; set; }
+    public int ttMax { get; set; }
+    public int fflitMin { get; set; }
+    public int fflitMax { get; set; }
+    public int fllitMin { get; set; }
+    public int fllitMax { get; set; }
+    public int ptlitMin { get; set; }
+    public int ptlitMax { get; set; }
+    public int lflitMin { get; set; }
+    public int lflitMax { get; set; }
+    public int lllitMin { get; set; }
+    public int lllitMax { get; set; }
+    public int tplitMin { get; set; }
+    public int tplitMax { get; set; }
+    public int ttlitMin { get; set; }
+    public int ttlitMax { get; set; }
+    public DateOnly DateOfReport { get; set; }
 }
